4c8b60b665110dcbd25b57f71f1a15b891689268
Ethan Episcope <ethan.episcope@duke.edu>
2024-09-06 02:51:39 -0400

    done with 3!

---
+++ 3TwoIntegerSum.py
@@ -0,0 +1,11 @@
+# Was gonna try to do dynamic programming, but i think you can't beat
+#O(n^2) runtime so why waste the space lol
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return 0
